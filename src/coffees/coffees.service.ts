fafc4eaf20ccb9a2794fb54ad7bde3fd91456af8
andarocks <aniruddhabhajan1994@gmail.com>
2023-12-16 19:26:02 +0530

    Updated controllers by calling service

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from 'src/coffees/entities/coffee.entity';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.coffees.findIndex((item) => item.id === +id);
+    if (existingCoffee >= 0) {
+      this.coffees[existingCoffee] = updateCoffeeDto;
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
