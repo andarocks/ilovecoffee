ffdb100f5145e2f056df15833c0bc2d39e7c099f
andarocks <aniruddhabhajan1994@gmail.com>
2023-12-16 18:43:19 +0530

    Added all types of controllers

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get()
+  findAll(@Query() paginatiionQuery) {
+    const { limit, offset } = paginatiionQuery;
+    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
+}
