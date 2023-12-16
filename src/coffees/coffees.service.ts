6caf4f2eedf76863e938b577d877bf58ce23b267
andarocks <aniruddhabhajan1994@gmail.com>
2023-12-16 19:42:58 +0530

    Updated service with exception

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Coffee } from 'src/coffees/entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +17,14 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find((item) => item.id === +id);
+    const coffee = this.coffees.find((item) => item.id === +id);
+
+    if (!coffee) {
+      throw new BadRequestException(
+        `Coffee #${id} not found. Please enter valid id.`,
+      );
+    }
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
