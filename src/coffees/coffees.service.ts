9724a634eabce077f9c53e19f25f3980c68854f3
andarocks <aniruddhabhajan1994@gmail.com>
2023-12-16 19:44:26 +0530

    Chapter2-2: Services

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,47 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Coffee } from 'src/coffees/entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+
+    if (!coffee) {
+      throw new BadRequestException(
+        `Coffee #${id} not found. Please enter valid id.`,
+      );
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.coffees.findIndex((item) => item.id === +id);
+    if (existingCoffee >= 0) {
+      this.coffees[existingCoffee] = updateCoffeeDto;
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
